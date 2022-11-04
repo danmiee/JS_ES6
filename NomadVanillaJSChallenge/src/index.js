99a0ef8cd25828e90483fd36b81a0527679897e1
danmiee <dan-mi@kakao.com>
2022-11-04 20:51:20 +0900

    노마드코더 바닐라JS 챌린지 졸업작품
    기본세팅 및 문제 기록

---
+++ NomadVanillaJSChallenge/src/index.js
@@ -0,0 +1,17 @@
+// 1. 브라우저에서 실시간으로 보이는 시계를 만들어야 합니다. ** [참고 자료 - #5 CLOCK]
+// (https://nomadcoders.co/javascript-for-beginners/lectures/2907)**
+
+// 2. 로컬 스토리지를 사용해 사용자 이름을 이용한 로그인 기능이 필요합니다. **
+// [참고 자료 - #4 LOGIN](https://nomadcoders.co/javascript-for-beginners/lectures/2899)**
+
+// 3. 로컬 스토리지를 사용한 투두리스트 기능이 필요합니다. **
+// 투두리스트는 새로 고침을 해도 지워지지 않아야 합니다. **
+// [참고 자료 - #7 TO DO LIST](https://nomadcoders.co/javascript-for-beginners/lectures/2914)**
+
+// 4. 배경 이미지가 랜덤하게 나타나야 합니다. **
+// 참고 자료 - #6 QUOTES AND BACKGROUND](https://nomadcoders.co/javascript-for-beginners/lectures/2911)**
+
+// 5. 위치 API를 사용해 현재 위치와 그에 맞는 날씨를 출력해야 합니다. **
+// [참고 자료 - #8 WEATHER](https://nomadcoders.co/javascript-for-beginners/lectures/2923)**
+
+// 6. 여러분의 CSS 실력을 뽐내주세요💖
